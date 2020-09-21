590313dc044077f4a83856b45d09361aa86cbd00
NataliaK546 <kioko.yoshi@gmail.com>
2020-09-21 03:53:00 +0300

    Таймер полностью переделан

--- AlarmTimer/AlarmTimer/Form1.Designer.cs
+++ AlarmTimer/AlarmTimer/Form1.Designer.cs
@@ -29,25 +29,11 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
-            this.button2 = new System.Windows.Forms.Button();
             this.button1 = new System.Windows.Forms.Button();
             this.maskedTextBox1 = new System.Windows.Forms.MaskedTextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.timer1 = new System.Windows.Forms.Timer(this.components);
-            this.timer2 = new System.Windows.Forms.Timer(this.components);
-            this.label3 = new System.Windows.Forms.Label();
             this.SuspendLayout();
-            // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(91, 316);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(116, 36);
-            this.button2.TabIndex = 9;
-            this.button2.Text = "Остановить";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
@@ -79,45 +65,18 @@
             this.label2.TabIndex = 6;
             this.label2.Text = "00:00";
             // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Baskerville Old Face", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(33, 19);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(240, 69);
-            this.label1.TabIndex = 5;
-            this.label1.Text = "00:00:00";
-            // 
             // timer1
             // 
             this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
-            // 
-            // timer2
-            // 
-            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Baskerville Old Face", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(91, 200);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(128, 50);
-            this.label3.TabIndex = 10;
-            this.label3.Text = "label3";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.label3);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.maskedTextBox1);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -127,15 +86,10 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.MaskedTextBox maskedTextBox1;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Timer timer1;
-        private System.Windows.Forms.Timer timer2;
-        private System.Windows.Forms.Label label3;
     }
 }
 
--- AlarmTimer/AlarmTimer/Form1.cs
+++ AlarmTimer/AlarmTimer/Form1.cs
@@ -24,65 +24,66 @@ namespace AlarmTimer
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            button2.Enabled = false;
-            timer01.Interval = 1000;
-            timer01.Tick += new EventHandler(timer1_Tick);
-            timer01.Start();
+           timer01.Interval = 1000;
+            
         }
 
-
-        string TimerTime;
-        DateTime TimeStart;
+        int tSec;
+        int tMin;
         private void button1_Click(object sender, EventArgs e)
         {
             if (b == false)
             {
-                label2.Text = "00:00";
-                TimerTime= maskedTextBox1.Text;
-                TimeStart = DateTime.Now;
-                timer2.Start();
+                label2.Text = maskedTextBox1.Text;
+               
+                tMin = Convert.ToInt32(maskedTextBox1.Text.Substring(0, 1));
+                tSec = Convert.ToInt32(maskedTextBox1.Text.Substring(3));
+                
                 maskedTextBox1.Visible = false;
                 button1.Text = "Убрать будильник";
                 b = true;
+                timer01.Tick += new EventHandler(timer1_Tick);
+                timer01.Start();
             }
   else if (b == true)
             {
                 label2.Text = "00:00";
-                timer2.Stop();
+                maskedTextBox1.Visible = true;
+                button1.Text = "Завести будильник";
+                b = false;
+                timer01.Stop();
+                timer01.Tick -= timer1_Tick;
+                
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (tSec > -1)
+            {
+                tSec--;
+            }
+            else
+            {
+                if (tMin != 0)
+                {
+                    tMin--;
+                    tSec = 59;
+                }
+                
+            }
+            label2.Text = tMin.ToString("00") + ":" +tSec.ToString("00");
+            if(tSec==0&&tMin==0)
+            {
+                timer01.Stop();
+                timer01.Tick -= timer1_Tick;
+                sp.Play();
+                label2.Text = "00:00";
                 maskedTextBox1.Visible = true;
                 button1.Text = "Завести будильник";
                 b = false;
             }
         }
 
-        private void timer2_Tick(object sender, EventArgs e)
-        {
-
-            if (TimerTime == label2.Text)
-            {
-                button2.Enabled = true;
-                sp.Play();
-            }
-
-        }
-
-        private void timer1_Tick(object sender, EventArgs e)
-        {
-           
-                label1.Text = DateTime.Now.Hour.ToString("00") + ":" +
-                    DateTime.Now.Minute.ToString("00") + ":" + DateTime.Now.Second.ToString("00");
-            var t = (DateTime.Now - TimeStart);
-            label2.Text =t.Minutes.ToString("00") + ":" + t.Seconds.ToString("00"); ;
-            
-        }
-
-        private void button2_Click(object sender, EventArgs e)
-        {
-            sp.Stop();
-          button2.Enabled = false;
-          maskedTextBox1.Visible = true;
-          button1.Text = "Завести будильник";
-          b = false;
-        }
     }
 }
